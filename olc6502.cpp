1408f772ec9f2ce5976d0ab248628cd91ea32a6b
theonelog <awni.alquraini@gmail.com>
2024-12-31 16:40:35 -0800

    did some more work on the cpu header file (olc6502.h)

--- olc6502.cpp
+++ olc6502.cpp
@@ -1,17 +1,28 @@
 #include "olc6502.h"
+#include "Bus.h"
 
 olc6502::olc6502(){
-
+    using olc = olc6502; //Creates a temporary naming variable to make the table more simple.
 }
 
 olc6502::~olc6502(){
 
 }
 
-uint8_t olc6502::read(uint16_t a){
-    return bus->read(a, false);
+
+
+uint8_t olc6502::read(uint16_t addr){
+    return bus->read(addr, false);
 }
 
-void olc6502::write(uint16_t a, uint8_t d){
-    bus->write(a, d);
+void olc6502::write(uint16_t addr, uint8_t dest){
+    bus->write(addr, dest);
+}
+
+void olc6502::clock(){
+    if(cycles == 0){
+        opcode = read(progc);
+        progc++;
+        cycles = lookup[opcode].cycles; //Gets the # of cycles that given operation needs to execute. *BEST CASE SCENARIO*
+    }
 }
--- olc6502.h
+++ olc6502.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <string>
+#include <vector>
 
 class Bus;
 
@@ -7,8 +9,6 @@ class olc6502{
     public:
         olc6502();
         ~olc6502();
-
-        void ConnectBus(Bus *bp) {bus  = bp;}
     public: 
         enum FLAGS6502{
             C = (1 << 0), //Carry
@@ -20,8 +20,43 @@ class olc6502{
             V = (1 << 6), //Overflow
             N = (1 << 7), //Negative
         };
+
+        uint8_t accum = 0x00; //Accumulator Register
+        uint8_t x = 0x00; //X Register
+        uint8_t y = 0x00; //Y Register
+        uint8_t stackp = 0x00; //Stack Pointer
+        uint16_t progc = 0x0000; //Status Register
+        uint8_t status = 0x00; //Reps the Status Register
+
+        void ConnectBus(Bus *bp) {bus  = bp;}
+        //All the possible addressing modes for the CPU
+        uint8_t IMP();
+        uint8_t ZP0();
+
+        void clock();
+        void reset();
+        void interruptReq(); //Can be ignored with the Disable Interruptions Flag is active
+        void nonMskInter(); //CAN NOT be ignored
+
+        uint8_t fetch();
+        uint8_t fetched = 0x00;
+
+        uint16_t addr_absolute = 0x0000;
+        uint16_t addr_relatvie = 0x00;
+        uint8_t opcode = 0x00;
+        uint8_t cycles = 0; 
+
     private:
         Bus *bus = nullptr;
         uint8_t read(uint16_t a);
         void write(uint16_t a, uint8_t d);
+        uint8_t getFlag(FLAGS6502 f);
+        void setFlag(FLAGS6502 f, bool v);
+        struct INSTRUCTION{
+            std::string name;
+            uint8_t(olc6502::*operate)(void) = nullptr; //The operation function that coresponds with the respective operation.
+            uint8_t(olc6502::*addrmode)(void) = nullptr; //The addressing mode that the function will use
+            uint8_t cycles = 0; //The # of cycles the instruction needs to fully execute.
+        };
+        std::vector<INSTRUCTION> lookup;
 };
