ca43bd2efce17fe28c78bfee338731785078a1c6
theonelog <169623207+theonelog@users.noreply.github.com>
2025-01-08 21:41:17 -0800

    setup all the opcodes in olc6502.h

--- olc6502.h
+++ olc6502.h
@@ -55,35 +55,63 @@ class olc6502{
         uint8_t ABY(); //Absolute Y
         uint8_t IND(); //Indirect  
     private:
-        //All OpCodes - https://www.masswerk.at/6502/6502_instruction_set.html#BPL - every column left to right bottom down before moving onto the next one.
-        uint8_t BRK(); /*Break*/                          
-        uint8_t BPL(); /*Branch on plus*/            
-        uint8_t JSR(); /*Jump to Subroutine*/             
-        uint8_t BMI(); /*Branch on Minus negative set*/   
-        uint8_t RTI(); /*Return from Interrupt*/          
-        uint8_t BVC(); /*Branch on Overflow Clear*/       
-        uint8_t RTS(); /*Return from Subroutine*/         
-        uint8_t BVS(); /*Branch on Overflow Set*/         
-        uint8_t BCC(); /*Branch on Carry Clear*/          
-        uint8_t LDY(); /*Load Y Register*/
-        uint8_t BCS(); /*Branch on Carry Set*/            
-        uint8_t CPY(); /*Compare Y Register*/             
-        uint8_t BNE(); /*Branch on Not Equal*/            
-        uint8_t CPX(); /*Compare X Register*/
-        uint8_t BEQ(); /*Branch on Equal*/
-        //Column 2
-        uint8_t ORA(); /*Or with accumulator*/
-        uint8_t AND();/*And*/
-        uint8_t EOR(); /*Exclusive Or*/
-        uint8_t ADC(); /*Add with Carry*/
-        uint8_t STA(); /*Store Accumulator*/
-        uint8_t LDA(); /*Load Accumulator*/
-        uint8_t CMP(); /*Compare with accumulator*/
-        uint8_t SBC(); /*Subtract with Carry*/
-        //Column 3
-        uint8_t LDX(); /*Load X Register*/
-        //Collumn 4
-        
+        //All OpCodes - https://www.masswerk.at/6502/6502_instruction_set.html#BPL
+        uint8_t ADC(); // add with carry
+        uint8_t AND(); // and (with accumulator)
+        uint8_t ASL(); // arithmetic shift left
+        uint8_t BCC(); // branch on carry clear
+        uint8_t BCS(); // branch on carry set
+        uint8_t BEQ(); // branch on equal (zero set)
+        uint8_t BIT(); // bit test
+        uint8_t BMI(); // branch on minus (negative set)
+        uint8_t BNE(); // branch on not equal (zero clear)
+        uint8_t BPL(); // branch on plus (negative clear)
+        uint8_t BRK(); // break / interrupt
+        uint8_t BVC(); // branch on overflow clear
+        uint8_t BVS(); // branch on overflow set
+        uint8_t CLC(); // clear carry
+        uint8_t CLD(); // clear decimal
+        uint8_t CLI(); // clear interrupt disable
+        uint8_t CLV(); // clear overflow
+        uint8_t CMP(); // compare (with accumulator)
+        uint8_t CPX(); // compare with X
+        uint8_t CPY(); // compare with Y
+        uint8_t DEC(); // decrement
+        uint8_t DEX(); // decrement X
+        uint8_t DEY(); // decrement Y
+        uint8_t EOR(); // exclusive or (with accumulator)
+        uint8_t INC(); // increment
+        uint8_t INX(); // increment X
+        uint8_t INY(); // increment Y
+        uint8_t JMP(); // jump
+        uint8_t JSR(); // jump subroutine
+        uint8_t LDA(); // load accumulator
+        uint8_t LDX(); // load X
+        uint8_t LDY(); // load Y
+        uint8_t LSR(); // logical shift right
+        uint8_t NOP(); // no operation
+        uint8_t ORA(); // or with accumulator
+        uint8_t PHA(); // push accumulator
+        uint8_t PHP(); // push processor status (SR)
+        uint8_t PLA(); // pull accumulator
+        uint8_t PLP(); // pull processor status (SR)
+        uint8_t ROL(); // rotate left
+        uint8_t ROR(); // rotate right
+        uint8_t RTI(); // return from interrupt
+        uint8_t RTS(); // return from subroutine
+        uint8_t SBC(); // subtract with carry
+        uint8_t SEC(); // set carry
+        uint8_t SED(); // set decimal
+        uint8_t SEI(); // set interrupt disable
+        uint8_t STA(); // store accumulator
+        uint8_t STX(); // store X
+        uint8_t STY(); // store Y
+        uint8_t TAX(); // transfer accumulator to X
+        uint8_t TAY(); // transfer accumulator to Y
+        uint8_t TSX(); // transfer stack pointer to X
+        uint8_t TXA(); // transfer X to accumulator
+        uint8_t TXS(); // transfer X to stack pointer
+        uint8_t TYA(); // transfer Y to accumulator
     private:
         Bus *bus = nullptr;
         uint8_t read(uint16_t a);
