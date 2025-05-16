1f5337a2af4e296c795e74ab6a815ebcda7736c8
theonelog <169623207+awnialq@users.noreply.github.com>
2025-05-15 21:27:31 -0700

    Refactor cartridge class; remove unused member variables and streamline memory management

--- cartridge.h
+++ cartridge.h
@@ -16,11 +16,12 @@ class cartridge{
         cartridge(std::string &fileName);
         ~cartridge();
     private: 
-        struct cartHeader *header;
-        uint32_t prgDataSize;
-        uint32_t chrDataSize;
-        std::ifstream fileStream;
-        std::string &fileName;
+        std::vector<uint8_t> prgMem;    //Program memory
+        std::vector<uint8_t> chrMem;    //Character memory
+
+        uint8_t mapperID = 0;
+        uint8_t numPrgBanks = 0;
+        uint8_t numChrBanks = 0;
     public:
         void loadRom();
     private:
