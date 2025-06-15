4fb469bff6e5a3a659e7166cd48bdeb0ddf477f3
theonelog <169623207+awnialq@users.noreply.github.com>
2025-06-15 10:41:17 -0700

    Refactor Mapper000 methods; streamline address mapping logic for CPU and PPU read/write operations

--- Mapper000.cpp
+++ Mapper000.cpp
@@ -6,18 +6,33 @@ Mapper000::~Mapper000(){}
 
 bool Mapper000::modCpuRead(uint16_t addr, uint32_t &mappedAddr){
     if(addr >= 0x8000 && addr <= 0xFFFF){
-        
+        mappedAddr = addr & (numPrgBanks > 1 ? 0x7FFF : 0x3FFF); //if there is more than one bank then mirror a larger range of addresses.
+        return true;
     }
+    return false;
 }
 
 bool Mapper000::modCpuWrite(uint16_t addr, uint32_t &mappedAddr){
-    
+    if(addr >= 0x8000 && addr <= 0xFFFF){
+        mappedAddr = addr & (numPrgBanks > 1 ? 0x7FFF : 0x3FFF); //if there is more than one bank then mirror a larger range of addresses.
+        return true;
+    }
+    return false;
 }
 
 bool Mapper000::modPpuRead(uint16_t addr, uint32_t &mappedAddr){
-    
+    //ppu has a very limited range so no extra mapping is needed for its functionality (just have to check if the addr is in range)
+    if(addr >= 0x0000 && 0x1FFF){
+        mappedAddr = addr;
+        return true;
+    }
+    return false;
 }
 
 bool Mapper000::modPpuWrite(uint16_t addr, uint32_t &mappedAddr){
-    
+    if(addr >= 0x0000 && 0x1FFF){
+        mappedAddr = addr;
+        return true;
+    }
+    return false;
 }
