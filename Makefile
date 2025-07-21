3d1c563968fae406c1153408a77b6b1336ee1e4b
theonelog <169623207+awnialq@users.noreply.github.com>
2025-07-21 14:10:39 -0700

    learning sdl

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = g++
-CXXFLAGS = -std=c++17 -Wall -Wextra -O2
+CXXFLAGS = -std=c++17 -Wall -Wextra -O2 $(shell sdl2-config --cflags)
 
 CPU_SOURCES = cpu6502.cpp Bus.cpp main.cpp
 CPU_OBJECTS = $(CPU_SOURCES:.cpp=.o)
@@ -8,7 +8,7 @@ CPU_TARGET = cpu6502-test
 all: $(CPU_TARGET)
 
 $(CPU_TARGET): $(CPU_OBJECTS)
-	$(CXX) $(CPU_OBJECTS) -o $@
+	$(CXX) $(CPU_OBJECTS) -o $@ $(shell sdl2-config --libs)
 
 %.o: %.cpp cpu6502.h Bus.h
 	$(CXX) $(CXXFLAGS) -c $< -o $@
--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include "cpu6502.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 int main(){
+if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("Hello SDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (!win) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(2000);  // Display window for 2 seconds
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+    /*
     cpu6502 *cpu = new cpu6502();
     cpu -> ~cpu6502();
-    return 0;
+    */
 }
 
